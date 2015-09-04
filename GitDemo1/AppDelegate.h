b277d35101160a5683fccf4e3f7924ba5b26041e
Mukum <laughingmukum@yahoo.com>
2015-09-04 12:17:13 +0800

    Initial Commit

---
+++ GitDemo1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitDemo1
+//
+//  Created by mukum on 4/9/15.
+//  Copyright (c) 2015 mukum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitDemo1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitDemo1
+//
+//  Created by mukum on 4/9/15.
+//  Copyright (c) 2015 mukum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
